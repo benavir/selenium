f28fbfdee52c74401b228a5b0fa44074d35f5b5c
yuriu_podporinov <yu.podporinov@rtk-dc.ru>
2020-09-13 12:10:54 +0300

    Задание 8. Проверка на наличие стикеров у товаров, и что он у каждого товара один

--- test_example.py
+++ test_example.py
@@ -3,12 +3,11 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-import time
 
 
 @pytest.fixture
 def driver(request):
-    wd = webdriver.Firefox()
+    wd = webdriver.Chrome()
     request.addfinalizer(wd.quit)
     return wd
 
@@ -22,7 +21,7 @@ def driver(request):
 #     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#content h1")))
 
 
-def test_example(driver):
+def test_side_menu(driver):
     driver.get("http://localhost/litecart/admin/")
     driver.find_element_by_name("username").send_keys("admin")
     driver.find_element_by_name("password").send_keys("admin")
@@ -38,3 +37,46 @@ def test_example(driver):
             driver.find_elements_by_css_selector('div>ul>li')[number].find_elements_by_css_selector('li>ul>li')[
                 number_mini].click()
             WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#content h1")))
+
+
+def test_stickers(driver):
+    driver.get("http://localhost/litecart/en/")
+    WebDriverWait(driver, 10).until(EC.title_is("Online Store | My Store"))
+    products = driver.find_elements_by_css_selector('li[class^=product]')
+    for duck in range(len(products)):
+        positions = []
+        all_prod = driver.find_elements_by_css_selector('li[class^=product]')[duck]
+        sticker = all_prod.find_elements_by_css_selector('div[class^=sticker]')
+        for labels in sticker:
+            text = labels.text
+            positions.append(text)
+        #     assert len(positions) == 1
+        # print(positions)
+        try:
+            if len(positions) == 1:
+                print(positions)
+            else:
+                print(positions, 'Too many labels')
+        except:
+            pass
+
+
+def test_price(driver):
+    driver.get("http://localhost/litecart/en/")
+    WebDriverWait(driver, 10).until(EC.title_is("Online Store | My Store"))
+    products = driver.find_elements_by_css_selector('li[class^=product]')
+    for duck in range(len(products)):
+        positions = []
+        all_prod = driver.find_elements_by_css_selector('li[class^=product]')[duck]
+        sticker = all_prod.find_elements_by_css_selector('div>*[class*=price]')
+        for dollars in sticker:
+            text = dollars.text
+            positions.append(text)
+        try:
+            if len(positions) == 1:
+                print(positions)
+            else:
+                print(positions, 'Too many prices')
+        except:
+            pass
+
